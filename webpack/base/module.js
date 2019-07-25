41e2c90faa204eff015ed0377e9f22ad16c00c48
qinzheng_wang <qinzheng_wang@ziguangcn.com>
2019-07-25 17:34:32 +0800

    less文件也做适配

--- webpack/base/module.js
+++ webpack/base/module.js
@@ -99,18 +99,64 @@ module.exports = {
       test: /\.less$/,
       use: extractLess.extract({
         fallback: 'style-loader',
-        use: ['css-loader', 'less-loader?javascriptEnabled=true']
+        use: [
+          'css-loader',
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              // Necessary for external CSS imports to work
+              // https://github.com/facebookincubator/create-react-app/issues/2677
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  overrideBrowserslist: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9' // React doesn't support IE8 anyway
+                  ],
+                  flexbox: 'no-2009'
+                }),
+                postcssAspectRatioMini({}),
+                postcssPxToViewport({
+                  viewportWidth: 750, // (Number) The width of the viewport.
+                  viewportHeight: 1334, // (Number) The height of the viewport.
+                  unitPrecision: 3, // (Number) The decimal numbers to allow the REM units to grow to.
+                  viewportUnit: 'vw', // (String) Expected units.
+                  selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px.
+                  minPixelValue: 1, // (Number) Set the minimum pixel value to replace.
+                  mediaQuery: false, // (Boolean) Allow px to be converted in media queries.
+                  exclude: /(\/|\\)(node_modules)(\/|\\)/
+                }),
+                postcssWriteSvg({
+                  utf8: false
+                }),
+                postcssCssnext({ warnForDuplicates: false }),
+                postcssViewportUnits({}),
+                cssnano({
+                  preset: 'advanced',
+                  autoprefixer: false,
+                  'postcss-zindex': false
+                })
+              ]
+            }
+          },
+          'less-loader?javascriptEnabled=true'
+        ]
       })
     },
     {
       test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-      use: [{
-        loader: 'url-loader',
-        options: {
-          limit: 5000,
-          name: 'images/[path]/[name].[ext]'
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 5000,
+            name: 'images/[path]/[name].[ext]'
+          }
         }
-      }]
+      ]
     }
   ]
 };
